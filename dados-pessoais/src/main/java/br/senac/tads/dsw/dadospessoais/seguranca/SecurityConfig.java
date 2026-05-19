d1f05ec36261e970f5fdc14cdf7bf5ca378d452a
Bryan Iuri Santos <bryanpolygon@gmail.com>
2026-05-18 22:39:41 -0300

    Projeto Finalizado

--- dados-pessoais/src/main/java/br/senac/tads/dsw/dadospessoais/seguranca/SecurityConfig.java
+++ dados-pessoais/src/main/java/br/senac/tads/dsw/dadospessoais/seguranca/SecurityConfig.java
@@ -7,13 +7,68 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// import org.springframework.
-// import org.springframework.
-// import org.springframework.
-// import org.springframework.
-// import org.springframework.
-// import org.springframework.
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
+    private final JwtFilter jwtFilter;
+    
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+
+        .csrf(csrf -> csrf.disable())
+
+        .headers(headers -> headers
+            .frameOptions(frame -> frame.disable()))
+
+        .sessionManagement(session -> session
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers(HttpMethod.POST, "/login").permitAll()
+            .requestMatchers("/h2-console/**").permitAll()
+            .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
+            .requestMatchers("/me").authenticated()
+            // Permitir acesso aos arquivos das telas - adiantando config da etapa 7.7
+            .requestMatchers("/*.html", "/*.css", "/*.js").permitAll()
+           
+            .anyRequest().authenticated()
+        )
+
+        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
+                
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
